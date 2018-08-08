63a1c3398784d976b920badd0b580edb6a625939
Dmitriy Kapustin <dmitriy.kapustin@comodo.com>
2018-08-08 15:44:13 +0300

    Simple script to pull all git subdirectories

---
+++ README.md
@@ -0,0 +1,1 @@
+Pull all git subdirectories
---
+++ pull_all.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Simple script to pull all git subdirectories
+
+# NOTE: if the -depth 1 option is not available, try -mindepth 1 -maxdepth 1.
+
+# find . -maxdepth 1 -type d -not -path "." -not -path ".." -exec echo $PWD/{} \;
+# find . -maxdepth 1 -type d -not -path "." -not -path ".." -exec git --git-dir={}/.git --work-tree=$PWD/{} status \;
+# find . -maxdepth 1 -type d -not -path "." -not -path ".." \( -exec sh -c 'echo Repo: $PWD/{}; false' \; -false -o -exec git --git-dir={}/.git --work-tree=$PWD/{} pull \; \)
+find . -maxdepth 1 -type d -not -path "." -not -path ".." -exec sh -c 'echo Repo: $PWD/{}; true' \; -exec git --git-dir={}/.git --work-tree=$PWD/{} pull \;
