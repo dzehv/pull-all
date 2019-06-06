811e85823a6be201ffb042fe6cf85c0f62696814
Dmitriy Kapustin <dmitriy.kapustin@nurd.com>
2019-06-06 13:59:16 +0300

    Colors fix 4 mac os

--- pull_all.sh
+++ pull_all.sh
@@ -63,17 +63,17 @@ fi
 # Since git 1.8.5 we can do the next thing
 # find . -maxdepth 1 -type d -not -path "." -not -path ".." -exec sh -c 'echo Repo: $PWD/{}; true' \; -exec git -C {} pull --no-edit \;
 # Update main repositories (always)
-echo "\e[92mUpdating repositories...\e[0m"
+echo "\033[92mUpdating repositories...\033[0m"
 find . -maxdepth 1 -type d -not -path "." -not -path ".." -exec sh -c 'echo Repo: $PWD/{}; true' \; -exec git --git-dir={}/.git --work-tree=$PWD/{} pull --no-edit \;
 
 # Prune non existing remote branches (-p arg)
 if [ "$PRUNE" -eq 1 ]; then
-    echo "\e[95mPrune non existing branches...\e[0m"
+    echo "\033[95mPrune non existing branches...\033[0m"
     find . -maxdepth 1 -type d -not -path "." -not -path ".." -exec sh -c 'echo Repo: $PWD/{}; true' \; -exec git --git-dir={}/.git --work-tree=$PWD/{} fetch -p --all \;
 fi
 
 # And submodules update (-s arg)
 if [ "$SUBMODULES" -eq 1 ]; then
-    echo "\e[91mUpdating submodules...\e[0m"
+    echo "\033[91mUpdating submodules...\033[0m"
     find . -type f -name '.gitmodules' | sed -r 's|/[^/]+$||' | sort | uniq | xargs -I % sh -c 'echo Repo: $PWD/%; git -C $PWD/% submodule update --remote --recursive --merge'
 fi
